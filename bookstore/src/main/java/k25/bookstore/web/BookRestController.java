25e721b74fc1eee6ed12ad0af2ecb3a1eaead29d
Jenni Muhonen <jenni@Otus>
2025-02-25 23:53:30 +0200

    lisätty rest controlleriin toimintoja

--- bookstore/src/main/java/k25/bookstore/web/BookRestController.java
+++ bookstore/src/main/java/k25/bookstore/web/BookRestController.java
@@ -1,9 +1,14 @@
 package k25.bookstore.web;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import k25.bookstore.domain.Book;
@@ -32,5 +37,31 @@ public class BookRestController {
 		System.out.println("Haetaan kirja id:llä " + id);
 		return bookRepository.findById(id);
 	}
+	
+	 // lisää uusi kirja
+    @PostMapping("books")
+    Book newBook(@RequestBody Book newBook) {
+        return bookRepository.save(newBook);
+    }
+    
+    // editoidaan
+    @PutMapping("/books/{id}")
+    Book editBook(@RequestBody Book editedBook, @PathVariable Long id) {
+        editedBook.setId(id);
+        return bookRepository.save(editedBook);
+    }
+    
+    //deletoidaan kirja
+    @DeleteMapping("/books/{id}")
+    public Iterable<Book> deleteCar(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return bookRepository.findAll();
+    }
+    
+    // etsi kirja tietyllä kielellä
+    @GetMapping("/books/category/{categoryName}")
+    List<Book> getBookByCategory(@PathVariable String categoryName) {
+        return bookRepository.findByCategoryCategoryName(categoryName);
+    }
 
 }
